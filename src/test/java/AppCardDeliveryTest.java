bdeedfc5d44d77225c086e9e3a665e0f123681a7
Alina <eparis@atlassian.com>
2025-02-12 13:12:12 +0300

    added README.md

--- src/test/java/AppCardDeliveryTest.java
+++ src/test/java/AppCardDeliveryTest.java
@@ -1,5 +1,7 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -10,17 +12,20 @@ import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 public class AppCardDeliveryTest {
     private String generateDate(int days, String pattern) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
     }
 
-    @Test
-    void test1() {
-        String planningDate = generateDate(3, "dd.MM.yyyy");
+    @BeforeEach
+    void setUp() {
         Selenide.open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldSendValidForm() {
+        String planningDate = generateDate(3, "dd.MM.yyyy");
         $("[data-test-id='city'] input").setValue("Волгоград");
         $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(planningDate);
@@ -28,13 +33,16 @@ public class AppCardDeliveryTest {
         $("[data-test-id='phone'] input").setValue("+71231231231");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(Selectors.withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Успешно! Встреча упешно забронирована на " + planningDate));
+
     }
 
+
     @Test
-    void test2() {
+    void shouldErrorIfCityEmpty() {
         String planningDate = generateDate(3, "dd.MM.yyyy");
-        Selenide.open("http://localhost:9999");
         $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Алина Осонова");
@@ -45,9 +53,8 @@ public class AppCardDeliveryTest {
     }
 
     @Test
-    void test3() {
+    void shouldErrorIfNameEmpty() {
         String planningDate = generateDate(3, "dd.MM.yyyy");
-        Selenide.open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Волгоград");
         $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(planningDate);
@@ -58,9 +65,8 @@ public class AppCardDeliveryTest {
     }
 
     @Test
-    void test4() {
+    void shouldErrorIfPhoneEmpty() {
         String planningDate = generateDate(8, "dd.MM.yyyy");
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Волгоград");
         $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(planningDate);
@@ -71,8 +77,7 @@ public class AppCardDeliveryTest {
     }
 
     @Test
-    void test5() {
-        Selenide.open("http://localhost:9999");
+    void shouldErrorIfDateIncorrect() {
         $("[data-test-id='city'] input").setValue("Волгоград");
         $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='name'] input").setValue("Алина Осонова");
@@ -84,9 +89,8 @@ public class AppCardDeliveryTest {
 
 
     @Test
-    void test6() {
+    void shouldErrorIfCheckboxRemoved() {
         String planningDate = generateDate(3, "dd.MM.yyyy");
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Волгоград");
         $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(planningDate);
@@ -97,9 +101,8 @@ public class AppCardDeliveryTest {
     }
 
     @Test
-    void test7() {
+    void shouldFindCityByTwoLetters() {
         String planningDate = generateDate(3, "dd.MM.yyyy");
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Во");
         $(By.xpath("//span[text()='Волгоград']")).click();
         $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
@@ -108,8 +111,10 @@ public class AppCardDeliveryTest {
         $("[data-test-id='phone'] input").setValue("+71231231231");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(Selectors.withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Успешно! Встреча упешно забронирована на" + planningDate));
+    }
     }
 
-}
 
